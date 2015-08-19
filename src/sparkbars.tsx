13328e8b1a86d886db8991042ada22b1993a9d43
Ian Glover <ian.glover@gmail.com>
2015-08-19 22:45:56 +0100

    Type annotations and more idiomatic TypeScript.

--- src/sparkbars.tsx
+++ src/sparkbars.tsx
@@ -16,12 +16,10 @@ class Sparkbars extends React.Component<SparkbarProperties, {}> {
         const min    = Math.min(0, Math.min.apply(Math, this.props.data));
         const max    = Math.max(0, Math.max.apply(Math, this.props.data));
         const range  = 2 * Math.max(Math.abs(min), Math.abs(max));
-        const scale  = function (y) { return y / range * max_height; };
-        const base   = function (y) { 
-                            return y < 0 ? zero_line : scale(max - y); 
-                        };
-        const height = function (y) { return scale(Math.abs(y)); };
-        const cls    = function (y) {
+        const scale  = (y: number) => y / range * max_height;
+        const base   = (y: number) => y < 0 ? zero_line : scale(max - y);
+        const height = (y: number) => scale(Math.abs(y));
+        const cls    = function (y: number): string {
                             var value = "bar";
                             if (y < 0) { value += " down"; }
                             else if (y > 0) { value += " up"; }
@@ -33,7 +31,7 @@ class Sparkbars extends React.Component<SparkbarProperties, {}> {
 
         var bars = [];
         for (var idx in this.props.data) {
-            var value = this.props.data[idx];
+            const value = this.props.data[idx];
             bars.push(<rect className={cls(value)} height={height(value)}
                             key={idx} title={value} width={width}
                             x={idx * (width+1)} y={base(value)}></rect>);
